2afba8bc589912e4614b9df11da5dd962a4e5a07
Dennis Omonywa <108891447+taz-denno@users.noreply.github.com>
2024-10-17 09:31:50 -0400

    Add files via upload

---
+++ environment/for-loop.py
@@ -0,0 +1,7 @@
+print("count to 10!")
+
+#Writing the for loop
+for x in range (0, 11):
+    print(x)
+    
+    
---
+++ environment/while-loop.py
@@ -0,0 +1,26 @@
+#Exercise 1: Working with a while loop
+print("Welcome to Guess Number!")
+print("The rules are simple. I will think of a number, and you will try to guess it.")
+
+#Importing random and writing a while loop
+import random
+number = random.randint(1,10)
+isGuessRight = False
+while isGuessRight != True:
+    guess = input("Guess a number between 1 and 10: ")
+    if int(guess) == number:
+        print("You guessed {}. That is correct! You win!".format(guess))
+        isGuessRight = True
+    else:
+        print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
+
+
+
+
+
+
+
+
+
+
+
